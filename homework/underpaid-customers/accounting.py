af754228d25746ef6c6d25f9b92d08e2a81011a0
Claudia Lopez <cklaudya1995@gmail.com>
2022-11-11 08:09:08 -0800

    finished hw

--- homework/underpaid-customers/accounting.py
+++ homework/underpaid-customers/accounting.py
@@ -60,31 +60,36 @@ if customer6_expected != customer6_paid:
           f"expected ${customer6_expected:.2f}"
           )
 
-mellon_cost = 1.00
+mellon_cost = 1.00 #universal variable
 
 def payment_status(payment_data_file):
+    '''Calculate cost of melon order to determine customer payment status.'''
 
-    payment_data = open(payment_data_file)
+    payment_data = open(payment_data_file) #we are opening a data file and assigning a variable to it.
 
-    for line in payment_data:
-        order = line.split('')
-        full_name = order[1]
-        first_name = full_name.split('')[0]
-        melons_qty = float(order[2])
+    for line in payment_data: #iterating the payment data file.
+        order = line.split('')  #the line is going to split on the white space to get a list of strings.
+        full_name = order[1] #assigning customers full name and assigning it the index of 1.
+        first_name = full_name.split('')[0] #customers full name will get split by the white space and we are assigning first name to the index 0.
+        #number of melons purchased is needed to calculate if what the customer paid was accurate.
+        melons_qty = float(order[2]) #float numbers can help us keep track of the money in dollars and cents.
         customer_payed = float(order[3])
+        #calculating price of the customer order.
         price = mellon_cost * melons_qty
-
+    #printing general customer order information.
     print(f"{first_name} payed {customer1_paid}, price expected was {price}")
+    #print payment status the if statement will print if they have underpaid or overpaid for the melons.
+    if customer1_paid != price:
 
-    if customer1_paid == price:
-        print(f'{first_name} has no balance')
-    elif customer1_paid < price:
-        print(f'{first_name} owes money')
-    elif customer1_paid > price:
-        print(f'{first_name} over payed')
+        if customer1_paid < price:
+            payment_status = 'underpaid'
+        elif customer1_paid > price:
+            payment_status = 'overpaid'
 
-    payment_data.close()
+        print(f"{first_name} has {payment_status} for their melons")
 
-    print(payment_status)
+    payment_data.close() #closing the file
+
+    print(payment_status) #calling the function
 
 
